e222f0789c208ce720045450626d1e2a6e5d084b
vapap <vpapapos@csd.auth.gr>
2021-04-29 19:58:16 +0300

    Every point is stored as an Object.

--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,27 @@
 public class Point {
 
-    public Point(int id, float lat, float lon){
+    private long id;
+    private int size;
+    private double lat, lon;
+    private String name = "";
 
+
+    public Point(long id, double lat, double lon){
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+        this.size = 24;
     }
 
-    public Point(int id, float lat, float lon, String)
-}
+    public Point(long id, double lat, double lon, String name) {
+        this(id, lat, lon);
+        this.name = name;
+        this.size += name.getBytes().length;
+    }
+
+    public String toString() {
+        return this.id + " " + this.lat + " " + this.lon + " " + this.name + "THE SIZE IS " + this.size;
+    }
+
+
+    }
