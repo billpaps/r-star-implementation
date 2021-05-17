59f05c616b3e5ed1b4b4daf69e5b2b728db7b738
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2021-05-17 13:33:34 +0300

    main.java

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,12 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args){
 
-        ArrayList<Point> points = new ArrayList<>();
-        String file_path = "file/points.csv";
-        String row;
-        String[] data;
-        Point new_point;
-
-//      Storing Points in an ArrayList. Every element is the List is an Object.
-        try {
-            Scanner csvReader = new Scanner(new FileReader(file_path));
-            while (csvReader.hasNextLine()) {
-                row = csvReader.nextLine();
-                data = row.split("\t");
-                try{
-                    new_point = new Point(data[0], data[1], data[2], data[3]);
-                }
-                catch (IndexOutOfBoundsException e){
-                    new_point = new Point(data[0], data[1], data[2]);
-                }
-                points.add(new_point);
-
-            }
-        }
-
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        int chunkSize = 0, counter = 0;
-        int blockId = 1;
-        try{
-            File datafile = new File("file/datafile.txt");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        DataFile data = new DataFile();
+        data.initialize();
 
 
-        String temp = "\n\n";
-//        Writing to datafile
-        try{
-            FileOutputStream writer = new FileOutputStream("file/datafile.txt");
-
-            for (int i = 0; i < points.size(); i++) {
-                if(chunkSize + points.get(i).get_size() <= 32768) {
-    //               Update chunksize
-                    chunkSize += points.get(i).get_size();
-    //                Write point i in the block
-                    writer.write(points.get(i).writeToFile());
-                }
-                else {
-                    writer.write(temp.getBytes());
-                    chunkSize = 0;
-                    blockId++;
-                    i--;
-                }
-                writer.close();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(blockId);
     }
 
 }
